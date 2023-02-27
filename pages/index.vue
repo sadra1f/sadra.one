a033538216fca78dd0e4fad8e1679d8d5223b35d
Sadra Saderi <sadrassx@gmail.com>
2023-02-27 22:43:15 +0330

    Add page content

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1 class="block">Hello!</h1>
+  <div class="flex flex-col w-screen h-screen justify-center items-center">
+    <div class="container text-center p-6 md:p-8">
+      <img
+        class="w-44 mb-6 mx-auto rounded-lg border"
+        src="https://github.com/Sadra1f.png"
+        alt="Image"
+      />
+
+      <h1 class="font-bold text-4xl">Sadra Saderi</h1>
+      <p>Web Developer / Computer Engineering Student</p>
+    </div>
+
+    <!-- prettier-ignore-attribute class -->
+    <div
+      class="absolute w-full bottom-8 p-6
+        md:p-8"
+    >
+      <!-- prettier-ignore-attribute class -->
+      <div
+        class="flex mx-auto w-full justify-between
+        sm:space-x-4 sm:w-3/4
+        md:w-1/2
+        lg:justify-center"
+      >
+        <a class="hover:underline" href="https://github.com/sadra1f/">GitHub</a>
+        <a
+          class="hover:underline"
+          href="https://www.linkedin.com/in/sadra-saderi/"
+        >
+          LinkedIn
+        </a>
+        <a class="hover:underline" href="https://instagram.com/sadra1f/">
+          Instagram
+        </a>
+        <a class="hover:underline" href="mailto:SadraSaderi@gmail.com">Email</a>
+      </div>
+    </div>
+  </div>
 </template>
